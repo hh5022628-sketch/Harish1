3c9d4f4a8f871c3f8e3f10b66bbb063e1747b74d
hh5022628-sketch <hh5022628@gmail.com>
2026-06-12 12:11:38 +0530

    logical operator

--- operators.js
+++ operators.js
@@ -15,3 +15,21 @@ console.log(c*=18)
 console.log(c/=10)
 console.log(c%=10)
 console.log(c**=10)
+
+//comparsion
+console.log("comparsion Operator")
+var a=10
+var b=20
+console.log(a<=b)
+console.log(a>=b)
+
+//logical operator
+console.log("Logical Operator")
+console.log(a > 0 && b > 0)
+console.log(a > 0 && b < 0)
+console.log(a > 0 || b > 0)
+console.log(!(a > 0));
+
+
+
+
