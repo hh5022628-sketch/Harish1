f524dfacee53b0c8875db95365ad2d57787a5d33
hh5022628-sketch <hh5022628@gmail.com>
2026-06-12 11:22:13 +0530

    Assignment

---
+++ operators.js
@@ -0,0 +1,17 @@
+//Arthimetic
+var a=15
+var b=10
+console.log(a-b);//subraction
+console.log(a*b);//multiplication
+console.log(a/b);//division
+console.log(a%b);//modulas
+console.log(a**b);//exponentiation
+
+//Assignment
+console.log("Assignment Opertor")
+var c=40
+console.log(c-=10)
+console.log(c*=18)
+console.log(c/=10)
+console.log(c%=10)
+console.log(c**=10)
---
+++ variables.js
@@ -0,0 +1,14 @@
+//var
+var name = "Harish"
+console.log(name)
+
+//let 
+{
+    let age = 22 
+    console.log(age);
+    
+}
+
+//const
+const dept = "IT"
+console.log(dept)
